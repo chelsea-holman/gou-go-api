bd7510eaa85c02aab4756da5b5eff8261fd2370a
Chelsea Holman <chel.holman@gmail.com>
2022-07-16 12:02:35 +0800

    dokku setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      post 'login', to: 'sessions#login', as: :login
       resources :spaces, only: [ :index ]
     end
   end
