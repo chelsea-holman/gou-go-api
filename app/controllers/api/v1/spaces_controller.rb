a61fc68e56dfbbf52586be050a04b0372fe4532c
Zahra Safdar <zahraalisafdar@yahoo.com>
2022-07-19 22:23:53 +0800

    show

--- app/controllers/api/v1/spaces_controller.rb
+++ app/controllers/api/v1/spaces_controller.rb
@@ -2,4 +2,21 @@ class Api::V1::SpacesController < Api::V1::BaseController
   def index
     @spaces = Space.all
   end
+  #config/routes.rb
+root to: "categories
+resources :categories
+
+#app/controllers/categories_controller.rb
+def index
+    @categories = Category.all
+end
+
+def show
+    @category = Category.find(params[:id])
+end
+
+#app/views/categories/restaurants
+<% for category in @categories do %>
+    <%= link_to category.name, category %>
+<% end %>
 end
