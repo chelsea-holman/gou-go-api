1dd0cfc7b3d8af81d932f08c97c8c78acd541243
Liya Tang <tangliya251@sohu.com>
2022-08-06 17:46:01 +0800

    added dogprofile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_134132) do
+ActiveRecord::Schema.define(version: 2022_08_06_072205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,10 +74,10 @@ ActiveRecord::Schema.define(version: 2022_08_04_134132) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "features", default: [], array: true
+    t.boolean "published", default: false
     t.string "sub_category"
     t.float "longitude"
     t.float "latitude"
-    t.boolean "published", default: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(version: 2022_08_04_134132) do
     t.string "open_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "dog_name"
+    t.string "picture_url"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
